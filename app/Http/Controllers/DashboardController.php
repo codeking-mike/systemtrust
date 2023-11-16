c980d9a00164017c8c7b7e590bf4c5d21deae6e8
micheal idam <princemicare@gmail.com>
2023-11-16 06:07:37 +0100

    ch

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,17 +20,17 @@ class DashboardController extends Controller
             'title' => 'Systemtrust ERP',
             'employees'=> Attendance::where('att_date', date('Y-m-d'))->get(),
             'todayAttendance'=> Attendance::where('att_date', date('Y-m-d'))->get(),
-            'tasklist'=>Task::where('fse_assigned', auth()->user()->name)->where('task_status', 'in progress')->get(),
+            'tasklist'=>Task::where('fse_assigned', auth()->user()->alias)->where('task_status', 'in progress')->get(),
             'tasks'=> $count=Task::where('task_status', 'in progress')->count(),
-            'mytasks'=> $count=Task::where('task_status', 'in progress')->where('fse_assigned', auth()->user()->name)->count(),
-            'myexpenses'=> $count=Expense::where('status', 'pending')->where('staff_name', auth()->user()->name)->count(),
+            'mytasks'=> $count=Task::where('task_status', 'in progress')->where('fse_assigned', auth()->user()->alias)->count(),
+            'myexpenses'=> $count=Expense::where('status', 'pending')->where('staff_name', auth()->user()->alias)->count(),
             'attendance'=> Attendance::where('staff_name', auth()->user()->name)->where('att_date', date('Y-m-d'))->get(),
             'total'=>$count=User::all()->count(),
             'present'=>$count=Attendance::where('att_date', date('Y-m-d'))->count(),
             'mynonsolar'=>$count=Machine::where('fse_assigned', auth()->user()->alias)->count(),
             'mysolar'=>$count=Solarmachine::where('fse_assigned',auth()->user()->alias)->count(),
             'myups'=>$count=Upsmachine::where('fse_assigned', auth()->user()->alias)->count(),
-            'notifications'=>Notification::latest()->where('receiver', auth()->user()->alias)->where('status', '0')->paginate(4)
+            'notifications'=>Notification::latest()->where('receiver',auth()->user()->alias)->orWhere('receiver', 'all')->paginate(4)
         ]);
     }
 }
