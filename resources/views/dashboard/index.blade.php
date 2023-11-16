37d7cee0e7ebb191a620c5bc92c1f25e5298e5ca
micheal idam <princemicare@gmail.com>
2023-11-16 06:29:34 +0100

    op

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -387,6 +387,63 @@
             </div>
           </div>
      @else
+     <div class="row">
+      <div class="col-md-12">
+        <div class="card mb-4">
+          <div class="card-header pb-0">
+            <h6>Co-Workers</h6>
+          </div>
+          <div class="card-body px-0 pt-0 pb-2">
+            <div class="table-responsive p-0">
+              <table class="table align-items-center mb-0">
+                <thead>
+                  <tr>
+                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Employee</th>
+                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Time-in</th>
+                    
+                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Time-Out</th>
+                    
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($todayAttendance as $employee)
+                      
+                
+                  <tr>
+                    <td>
+                      <div class="d-flex px-2 py-1">
+                        
+                        <div class="d-flex flex-column justify-content-center">
+                          <h6 class="mb-0 text-sm">{{$employee->staff_name }}</h6>
+                          
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                     
+                      <p class="text-xs font-weight-bold mb-0">{{$employee->time_in }}</p>
+                      
+                    </td>
+                    <td>
+                      @php
+                          
+                      @endphp
+                      <p class="text-xs font-weight-bold mb-0">{{$employee->time_out }}</p>
+                      
+                    </td>
+                    
+                   
+                   
+                  </tr>
+                  @endforeach
+                  
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
           <div class="row">
             <div class="col-md-12">
               <div class="card mb-4">
